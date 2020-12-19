45395c0c264593c62664c4fee01e50b8476c63bf
jennarsimon <jrs222@lehigh.edu>
2020-12-18 21:59:38 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// setup server
+var express = require('express');
+var app = express();
+app.listen(3000, () => {
+   console.log('Server listening on 3000');
+})
+
+// allow use of .env
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env' });
+
+// connect to MongoDB
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+   console.log('Connected to Mongo DB Successfully');
+})
+
+// allow parsing of JSON
+const bodyParser = require('body-parser');
+app.use( bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set up user routes
+const usersRoute = require('./routes/user.js');
+app.use('/users' , usersRoute);
+
+// direct to login page
+app.get('/',function(req,res){
+   res.sendFile(__dirname+'/views/signup.html');
+});
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+var express = require('express');
+const router = express.Router();
+const User = require('../model/User.js'); 
+const bcrypt = require('bcrypt');
+
+router.post('/users', (request, response) => {
+
+});
+
+module.exports = router;
