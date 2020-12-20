b7614aff79e91972907771da20e53d733a8c9333
jennarsimon <jrs222@lehigh.edu>
2020-12-19 20:06:51 -0600

    creating new user works

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,25 @@ const router = express.Router();
 const User = require('../model/User.js'); 
 const bcrypt = require('bcrypt');
 
-router.post('/users', (request, response) => {
+router.post('/', (request, response) => {
+    const user = new User({
+       firstName : request.body.firstName,
+       lastName : request.body.lastName,
+       userName : request.body.userName,
+       password : request.body.password,
+       email : request.body.email
+    });
+    bcrypt.hash(user.password, 10, function (err, hash){
+        if (err) { 
+        return next(err);
+        }
+        user.password = hash;
+        user.save().then(data => {
+            console.log('Successfully created a new User');
+        })
+    }).catch(error => {
+        response.status(500).send('error creating user');
+    });
 
 });
 
