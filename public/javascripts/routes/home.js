3846112dac144665f4b6bfb638ea0f2738093285
jennarsimon <jrs222@lehigh.edu>
2020-12-31 14:22:06 -0600

    styled some home page

--- public/javascripts/routes/home.js
+++ public/javascripts/routes/home.js
@@ -3,4 +3,14 @@ window.onload = function () {
 
     const username = window.location.pathname.split("/").pop();
     console.log('username', username);
+
+    let name = document.getElementsByTagName('b')[0];
+    console.log('name', name);
+
+    fetch('/users/' + username)
+    .then(response => response.json())
+    .then(data => {
+        name.innerHTML = data.firstName + '!';
+    })
+
 }
