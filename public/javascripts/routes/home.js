30f54d65c2407a506fba6d8df013d3f02ef79f6e
jennarsimon <jrs222@lehigh.edu>
2021-01-04 10:43:40 -0600

    changed welcome banner design

--- public/javascripts/routes/home.js
+++ public/javascripts/routes/home.js
@@ -4,13 +4,13 @@ window.onload = function () {
     const username = window.location.pathname.split("/").pop();
     console.log('username', username);
 
-    let name = document.getElementsByTagName('b')[0];
+    let name = document.getElementsByTagName('h3')[0];
     console.log('name', name);
 
     fetch('/users/' + username)
     .then(response => response.json())
     .then(data => {
-        name.innerHTML = data.firstName + '!';
+        name.innerHTML = name.innerHTML + data.firstName + '!';
     })
 
 }
